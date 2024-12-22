e6b2de55dcb2da61a77b78fc9650b370968cb12f
DarkPhoeniix <marina.vovchuk777@gmail.com>
2024-12-22 10:11:40 +0200

    Removed unused parameter in ResizeEvent

--- Renderer/Events/ResizeEvent.h
+++ Renderer/Events/ResizeEvent.h
@@ -9,15 +9,13 @@ namespace Core
         class ResizeEvent : public IEvent
         {
         public:
-            ResizeEvent(int width, int height, bool fullscreen = false)
+            ResizeEvent(int width, int height)
                 : width(width)
                 , height(height)
-                , isFullscreen(fullscreen)
             {   }
 
             int width;      // The new width of the window
             int height;     // The new height of the window
-            bool isFullscreen;
         };
     } // namespace Events
 } // namespace Core
