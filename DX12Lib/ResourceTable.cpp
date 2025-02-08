45ccde35e1b391953d8ba1f533ce4d00b5858bfc
DarkPhoeniix <marina.vovchuk777@gmail.com>
2025-02-08 09:40:49 +0200

    Minor fix in resource table placement

--- DX12Lib/ResourceTable.cpp
+++ DX12Lib/ResourceTable.cpp
@@ -75,7 +75,7 @@ namespace dx12
         ResourceKey key = { resource->GetName(), viewType };
         InternalResourceDesc value = { resource, descriptorHeap.GetCurrentOffset(), viewType};
 
-        resources.insert(std::make_pair(key, value));
+        resources.insert_or_assign(key, value);
         switch (viewType)
         {
         case ResourceViewType::RTV:
