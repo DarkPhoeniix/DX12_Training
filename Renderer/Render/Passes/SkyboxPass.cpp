67ed9f7e11ce1c2518970216ac0237a4302dde32
DarkPhoeniix <marina.vovchuk777@gmail.com>
2025-02-28 19:50:27 +0200

    Slightly improved skybox render pass

--- Renderer/Render/Passes/SkyboxPass.cpp
+++ Renderer/Render/Passes/SkyboxPass.cpp
@@ -28,45 +28,42 @@ namespace render
         task->SetName("skybox");
         _tasks.push_back(task);
 
-        std::shared_ptr<scene::Entity> entity = _scene->FindNodeByComponentName("Skybox");
-        if (!entity)
-        {
-            return;
-        }
-
-        scene::Skybox* skybox = entity->GetComponentAs<scene::Skybox>("Skybox");
-
         dx12::CommandList& commandList = *task->GetCommandLists().front();
         commandList.SetName("Render skybox command list");
 
-        PIXBeginEvent(commandList.GetDXCommandList().Get(), 3, "Skybox");
+        if (std::shared_ptr<scene::Entity> entity = _scene->FindNodeByComponentName("Skybox"))
         {
-            commandList.SetPipelineState(_skyboxPipeline);
+            scene::Skybox* skybox = entity->GetComponentAs<scene::Skybox>("Skybox");
 
-            helpers::SetupSceneDataGPU(*_scene, commandList, _frame);
+            PIXBeginEvent(commandList.GetDXCommandList().Get(), 3, "Skybox");
+            {
+                commandList.SetPipelineState(_skyboxPipeline);
 
-            dx12::ResourceTable& sceneTable = *_scene->GetCache().GetTextureTable();
-            dx12::ResourceTable& frameTable = _frame->GetResourceTable();
+                helpers::SetupSceneDataGPU(*_scene, commandList, _frame);
 
-            dx12::Resource* target = frameTable.GetResourceByName("HDR_Lightpass", dx12::ResourceViewType::UAV);
-            dx12::Resource* skyboxTexture = skybox->SkydomeTexture.get();
-            dx12::Resource* depth = &_gBuffer->GetDepthTexture();
-            
-            frameTable.CopyDescriptor(skyboxTexture, dx12::ResourceViewType::SRV, sceneTable);
+                dx12::ResourceTable& sceneTable = *_scene->GetCache().GetTextureTable();
+                dx12::ResourceTable& frameTable = _frame->GetResourceTable();
 
-            _frame->BindDescriptorHeaps(commandList);
+                dx12::Resource* target = frameTable.GetResourceByName("HDR_Lightpass", dx12::ResourceViewType::UAV);
+                dx12::Resource* skyboxTexture = skybox->SkydomeTexture.get();
+                dx12::Resource* depth = &_gBuffer->GetDepthTexture();
 
-            commandList.SetDescriptorTable(3, frameTable.GetResourceGPUHandle(depth, dx12::ResourceViewType::SRV));
-            commandList.SetDescriptorTable(4, frameTable.GetResourceGPUHandle(skyboxTexture, dx12::ResourceViewType::SRV));
-            commandList.SetDescriptorTable(5, frameTable.GetResourceGPUHandle(target, dx12::ResourceViewType::UAV));
+                frameTable.CopyDescriptor(skyboxTexture, dx12::ResourceViewType::SRV, sceneTable);
 
-            DirectX::XMUINT2 viewportSize = _activeCamera->GetViewport().GetSize();
-            int xThreadGroups = (uint32_t)std::ceilf(viewportSize.x / 8.0f);
-            int yThreadGroups = (uint32_t)std::ceilf(viewportSize.y / 8.0f);
+                _frame->BindDescriptorHeaps(commandList);
 
-            commandList.Dispatch(xThreadGroups, yThreadGroups);
+                commandList.SetDescriptorTable(3, frameTable.GetResourceGPUHandle(depth, dx12::ResourceViewType::SRV));
+                commandList.SetDescriptorTable(4, frameTable.GetResourceGPUHandle(skyboxTexture, dx12::ResourceViewType::SRV));
+                commandList.SetDescriptorTable(5, frameTable.GetResourceGPUHandle(target, dx12::ResourceViewType::UAV));
+
+                DirectX::XMUINT2 viewportSize = _activeCamera->GetViewport().GetSize();
+                int xThreadGroups = (uint32_t)std::ceilf(viewportSize.x / 8.0f);
+                int yThreadGroups = (uint32_t)std::ceilf(viewportSize.y / 8.0f);
+
+                commandList.Dispatch(xThreadGroups, yThreadGroups);
+            }
+            PIXEndEvent(commandList.GetDXCommandList().Get());
         }
-        PIXEndEvent(commandList.GetDXCommandList().Get());
 
         commandList.Close();
     }
