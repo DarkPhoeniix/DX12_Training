9d2189313bd1e7cd0b1e6830b2ec8eeaceb33a9b
DarkPhoeniix <marina.vovchuk777@gmail.com>
2025-03-15 20:52:51 +0200

    another minor

--- Renderer/Render/Frame/CacheGPU.h
+++ Renderer/Render/Frame/CacheGPU.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "Heap.h"
-
-#include <mutex>
+#include <shared_mutex>
 
 class CacheGPU
 {
@@ -11,24 +9,24 @@ public:
     {
         void* DataCPU = nullptr;
         D3D12_GPU_VIRTUAL_ADDRESS DataGPU = 0;
-        uint32_t Offset = -1;
+        std::uint32_t Offset = (std::uint32_t)-1;
     };
 
     void SetResource(std::shared_ptr<dx12::Resource> memoryBlock);
     void Clear();
 
-    DataHandle RequestPlacement(const std::string& name, uint32_t size);
-    DataHandle GetOrPlaceResource(const std::string& name, uint32_t size);
+    DataHandle RequestPlacement(const std::string& name, std::uint32_t size);
+    DataHandle GetOrPlaceResource(const std::string& name, std::uint32_t size);
     DataHandle GetResourcePlacement(const std::string& name);
 
     std::shared_ptr<dx12::Resource> GetCache();
 
 private:
-    std::mutex _cacheMutex;
-
     std::unordered_map<std::string, DataHandle> _placedResources;
     std::shared_ptr<dx12::Resource> _cache;
 
-    uint32_t _size;
-    uint32_t _currentOffset;
+    std::uint32_t _size;
+    std::uint32_t _currentOffset;
+
+    std::shared_mutex _mutex;
 };
