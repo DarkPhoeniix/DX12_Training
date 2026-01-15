00cc7d69596e9f8274f8511bf8768cd65c5cf1b5
DarkPhoeniix <marina.vovchuk777@gmail.com>
2026-01-15 18:24:55 +0100

    Added shader debug info and excluded shaders from MSVC build

--- Scripts/CompileShaderDxc.cmake
+++ Scripts/CompileShaderDxc.cmake
@@ -1,12 +1,16 @@
 
-function(CompileShader output_directory shader_path shader_type optimization_level)
+function(CompileShader output_directory shader_path shader_type optimization_level debug_info)
     get_filename_component(shader_name ${shader_path} NAME_WE)
     message("Compiling shader: ${shader_name} as type: ${shader_type} with optimization: ${optimization_level}")
-    
-    message("dxc.exe /E\"main\" /T\"${shader_type}_6_6\" -${optimization_level} /Fo\"${output_directory}/${shader_name}.cso\" ${shader_path}")
+
     add_custom_command(
         TARGET Renderer POST_BUILD
-        COMMAND dxc.exe /E main /T ${shader_type}_6_6 -${optimization_level} /Fo ${output_directory}/${shader_name}.cso ${shader_path}
+        COMMAND dxc.exe 
+                /E main 
+                /T ${shader_type}_6_6 
+                -${optimization_level} 
+                /Fo ${output_directory}/${shader_name}.cso ${shader_path}
+                $<$<BOOL:${debug_info}>:/Zi>
         VERBATIM
     )
 endfunction()
